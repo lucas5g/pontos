e860f97aae963c4d9d9c5644bc93f941c004baaa
lucas.assuncao <lucas.assuncao@defensoria.mg.def.br>
2025-01-22 09:41:16 -0300

    mudei para express

--- src/components/Input.tsx
+++
@@ -1,9 +0,0 @@
-interface Props extends React.InputHTMLAttributes<HTMLInputElement>{
-
-}
-export function Input(...props: Props[]){
-    // if (typeof window === 'undefined') return null
-    return (
-        <input className="rounded p-2 bg-gray-600 " {...props} />
-    )
-}
